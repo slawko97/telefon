b9ff8f663beca9bee64a0afe0433d7b58d2cbb89
slawko97 <34061395+slawko97@users.noreply.github.com>
2018-01-22 15:14:06 +0100

    Update TelefonJavaFX.java

--- TelefonJavaFX.java
+++ TelefonJavaFX.java
@@ -10,7 +10,6 @@ import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -27,26 +26,30 @@ import javafx.stage.Stage;
 public class TelefonJavaFX extends Application {
     
     Telefon tel = new Telefon();
+    Stage primaryStage;
     TextField zadzwonTextField;
     TextField numerTelefonuWyslijSmsTextField;
     TextField trescWyslijSmsTextField;
     TextArea przegladajTextArea;
     Label komunikatLabel;
+    Button generujPolaczenieButton;
     Button odbierzPolaczenieButton;
     Button odrzucPolaczenieButton;
     Button zakonczPolaczenieButton;
+    Scene glownaScene;
+    Scene polaczenieScene;
     
     @Override
-    public void start(Stage primaryStage) {
+    public void start(final Stage primaryStage) {
         
         // Tworzenie siatki pionowej
-        VBox siatkaPionowaVBox = new VBox(5);
-        siatkaPionowaVBox.setPadding(new Insets(5));
+        VBox glownaVBox = new VBox(5);
+        glownaVBox.setPadding(new Insets(5));
         
         // Tworzenie etykiety zadzwon
         Label zadzwonLabel = new Label();
         zadzwonLabel.setText("Zadzwon");
-        siatkaPionowaVBox.getChildren().add(zadzwonLabel);
+        glownaVBox.getChildren().add(zadzwonLabel);
         
         // Tworzenie siatki poziomej zadzwon
         HBox zadzwonHBox = new HBox(5);
@@ -77,12 +80,12 @@ public class TelefonJavaFX extends Application {
         zadzwonHBox.getChildren().add(zadzwonButton);
         
         // Dodanie siatki poziomej zadzwon do siatki pionowej
-        siatkaPionowaVBox.getChildren().add(zadzwonHBox);
+        glownaVBox.getChildren().add(zadzwonHBox);
         
         // Tworzenie etykiety wyslij sms
         Label wyslijSmsLabel = new Label();
         wyslijSmsLabel.setText("Wyslij sms");
-        siatkaPionowaVBox.getChildren().add(wyslijSmsLabel);
+        glownaVBox.getChildren().add(wyslijSmsLabel);
         
         // Tworzenie siatki poziomej numer telefonu wyslij sms
         HBox numerTelefonuWyslijSmsHBox = new HBox(5);
@@ -101,7 +104,7 @@ public class TelefonJavaFX extends Application {
         numerTelefonuWyslijSmsHBox.getChildren().add(numerTelefonuWyslijSmsTextField);
         
         // Dodanie siatki poziomej numer telefonu wyslij sms do siatki pionowej
-        siatkaPionowaVBox.getChildren().add(numerTelefonuWyslijSmsHBox);
+        glownaVBox.getChildren().add(numerTelefonuWyslijSmsHBox);
         
         // Tworzenie siatki poziomej tresc wyslij sms
         HBox trescWyslijSmsHBox = new HBox(5);
@@ -132,7 +135,7 @@ public class TelefonJavaFX extends Application {
         trescWyslijSmsHBox.getChildren().add(wyslijSmsButton);
         
         // Dodanie siatki poziomej tresc wyslij sms do siatki pionowej
-        siatkaPionowaVBox.getChildren().add(trescWyslijSmsHBox);
+        glownaVBox.getChildren().add(trescWyslijSmsHBox);
         
         // Tworzenie siatki poziomej przegladaj historie polaczen
         HBox przegladajHistoriePolaczenHBox = new HBox(5);
@@ -156,7 +159,7 @@ public class TelefonJavaFX extends Application {
         przegladajHistoriePolaczenHBox.getChildren().add(przegladajHistoriePolaczenButton);
         
         // Dodanie siatki poziomej przegladaj historie polaczen do siatki pionowej
-        siatkaPionowaVBox.getChildren().add(przegladajHistoriePolaczenHBox);
+        glownaVBox.getChildren().add(przegladajHistoriePolaczenHBox);
         
         // Tworzenie siatki poziomej przegladaj tresci sms
         HBox przegladajTresciSmsHBox = new HBox(5);
@@ -180,84 +183,52 @@ public class TelefonJavaFX extends Application {
         przegladajTresciSmsHBox.getChildren().add(przegladajTresciSmsButton);
         
         // Dodanie siatki poziomej przegladaj tresci sms do siatki pionowej
-        siatkaPionowaVBox.getChildren().add(przegladajTresciSmsHBox);
+        glownaVBox.getChildren().add(przegladajTresciSmsHBox);
         
         // Tworzenie pola edycyjnego przegladaj
         przegladajTextArea = new TextArea();
         przegladajTextArea.setWrapText(true);
         przegladajTextArea.setEditable(false);
         przegladajTextArea.setMaxHeight(150);
-        siatkaPionowaVBox.getChildren().add(przegladajTextArea);
+        glownaVBox.getChildren().add(przegladajTextArea);
         
         // Tworzenie etykiety komunikat
         komunikatLabel = new Label();
         komunikatLabel.setText("Brak powiadomien");
         komunikatLabel.setWrapText(true);
-        siatkaPionowaVBox.getChildren().add(komunikatLabel);
+        glownaVBox.getChildren().add(komunikatLabel);
         
-        // Tworzenie siatki poziomej polaczenie
-        HBox polaczenieHBox = new HBox(5);
+        // Tworzenie siatki poziomej generuj
+        HBox generujHBox = new HBox(5);
         
         // Tworzenie przycisku generuj polaczenie
-        Button generujPolaczenieButton = new Button("Generuj Polaczenie");
+        generujPolaczenieButton = new Button("Generuj Polaczenie");
         generujPolaczenieButton.setOnAction(new EventHandler<ActionEvent>() {
             
             @Override
             public void handle(ActionEvent event) {
                 tel.generujPolaczenie();
-                komunikatLabel.setText("Otrzymano polaczenie"+tel.getPolaczenie(0).toString());
-                odbierzPolaczenieButton.setVisible(true);
-                odrzucPolaczenieButton.setVisible(true);
+                komunikatLabel.setText("Otrzymano polaczenie "+tel.getPolaczenie(tel.polaczenia.size()-1).toString());
+                primaryStage.setScene(polaczenieScene);
             }
         });
         
-        siatkaPionowaVBox.getChildren().add(generujPolaczenieButton);
-        
-        // Tworzenie przycisku odbierz polaczenie
-        odbierzPolaczenieButton = new Button("Odbierz");
-        odbierzPolaczenieButton.setVisible(false);
-        odbierzPolaczenieButton.setOnAction(new EventHandler<ActionEvent>() {
+        //Tworzenie przycisku generuj sms
+        Button generujSmsButton = new Button("Generuj Sms");
+        generujSmsButton.setOnAction(new EventHandler<ActionEvent>() {
             
             @Override
             public void handle(ActionEvent event) {
-                //tel.odbierzPolaczenie(0);
-                odbierzPolaczenieButton.setVisible(false);
-                odrzucPolaczenieButton.setVisible(false);
-                zakonczPolaczenieButton.setVisible(true);
+                tel.generujSms();
+                komunikatLabel.setText("Otrzymano sms "+tel.getSms(tel.smsy.size()-1).toString());
             }
         });
         
-        // Tworzenie przycisku odrzuc polaczenie
-        odrzucPolaczenieButton = new Button("Odrzuc");
-        odrzucPolaczenieButton.setVisible(false);
-        odrzucPolaczenieButton.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                //tel.odrzucPolaczenie(0);
-                odbierzPolaczenieButton.setVisible(false);
-                odrzucPolaczenieButton.setVisible(false);
-            }
-        });
+        // Dodanie kontrolek do siatki poziomej generuj
+        generujHBox.getChildren().addAll(generujPolaczenieButton, generujSmsButton);
         
-        // Tworzenie przycisku zakoncz polaczenie
-        zakonczPolaczenieButton = new Button("Zakoncz");
-        zakonczPolaczenieButton.setVisible(false);
-        zakonczPolaczenieButton.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                zakonczPolaczenieButton.setVisible(false);
-            }
-        });
-        
-        // Dodanie przyciskow do siatki poziomej polaczenie
-        polaczenieHBox.getChildren().add(odbierzPolaczenieButton);
-        polaczenieHBox.getChildren().add(odrzucPolaczenieButton);
-        polaczenieHBox.getChildren().add(zakonczPolaczenieButton);
-        
-        // Dodanie siatki poziomej polaczenie do siatki pionowej
-        siatkaPionowaVBox.getChildren().add(polaczenieHBox);
+        // Dodanie siatki poziomej generuj do siatki pionowej
+        glownaVBox.getChildren().add(generujHBox);
         
         // Tworzenie przycisku zakoncz
         Button zakonczButton = new Button("Zakoncz");
@@ -269,14 +240,68 @@ public class TelefonJavaFX extends Application {
             }
         });
         
-        siatkaPionowaVBox.getChildren().add(zakonczButton);
+        glownaVBox.getChildren().add(zakonczButton);
         
-        // Tworzenie sceny
-        Scene scene = new Scene(siatkaPionowaVBox, 400, 600);
+        // Tworzenie siatki pionowej
+        VBox polaczenieVBox = new VBox(5);
+        polaczenieVBox.setPadding(new Insets(5));
+        
+        // Tworzenie siatki poziomej polaczenie
+        HBox polaczenieHBox = new HBox(5);
+        
+        // Tworzenie przycisku odbierz polaczenie
+        odbierzPolaczenieButton = new Button("Odbierz");
+        odbierzPolaczenieButton.setOnAction(new EventHandler<ActionEvent>() {
+            
+            @Override
+            public void handle(ActionEvent event) {
+                tel.odbierzPolaczenie(tel.polaczenia.size()-1);
+                odbierzPolaczenieButton.setVisible(false);
+                odrzucPolaczenieButton.setVisible(false);
+                zakonczPolaczenieButton.setVisible(true);
+            }
+        });
+        
+        // Tworzenie przycisku odrzuc polaczenie
+        odrzucPolaczenieButton = new Button("Odrzuc");
+        odrzucPolaczenieButton.setOnAction(new EventHandler<ActionEvent>() {
+            
+            @Override
+            public void handle(ActionEvent event) {
+                tel.odrzucPolaczenie(tel.polaczenia.size()-1);
+                primaryStage.setScene(glownaScene);
+            }
+        });
+        
+        // Tworzenie przycisku zakoncz polaczenie
+        zakonczPolaczenieButton = new Button("Zakoncz");
+        zakonczPolaczenieButton.setVisible(false);
+        zakonczPolaczenieButton.setOnAction(new EventHandler<ActionEvent>() {
+            
+            @Override
+            public void handle(ActionEvent event) {
+                zakonczPolaczenieButton.setVisible(false);
+                odbierzPolaczenieButton.setVisible(true);
+                odrzucPolaczenieButton.setVisible(true);
+                primaryStage.setScene(glownaScene);
+            }
+        });
+        
+        // Dodanie przyciskow do siatki poziomej polaczenie
+        polaczenieHBox.getChildren().add(odbierzPolaczenieButton);
+        polaczenieHBox.getChildren().add(odrzucPolaczenieButton);
+        polaczenieHBox.getChildren().add(zakonczPolaczenieButton);
+        
+        // Dodanie siatki poziomej polaczenie do siatki pionowej
+        polaczenieVBox.getChildren().add(polaczenieHBox);
+        
+        // Tworzenie scen
+        glownaScene = new Scene(glownaVBox, 400, 500);
+        polaczenieScene = new Scene(polaczenieVBox, 400, 500);
         
         // Ustalenie tytulu, glownej sceny i pokazanie glownej sceny
         primaryStage.setTitle("Telefon");
-        primaryStage.setScene(scene);
+        primaryStage.setScene(glownaScene);
         primaryStage.show();
     }
 
